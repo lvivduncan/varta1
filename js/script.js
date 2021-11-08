870ec679d99a5f1bbe8a635cbb25107305c7d9de
lvivduncan <lvivduncan@gmail.com>
2021-11-08 10:41:42 +0200

    start home.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 5-11-2021
+
+// клік на кнопці -- відкриває форму пошуку
+const searchButton = document.getElementById('search-button')
+
+searchButton.addEventListener('click', openSearchForm)
+
+function openSearchForm(){
+    // TODO: ховає меню, показує форму
+}
+
