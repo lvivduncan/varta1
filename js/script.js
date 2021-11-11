7e75ff5251b2ebee1646ef82251a729b871e2d7b
lvivduncan <lvivduncan@gmail.com>
2021-11-11 16:11:10 +0200

    mobile finish

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 // 5-11-2021
 
 // клік на кнопці -- відкриває форму пошуку
-const searchButton = document.getElementById('search-button')
+document.getElementById('search-button').addEventListener('click', () => {
+    
+})
 
-searchButton.addEventListener('click', openSearchForm)
+// показати мобільне меню
+document.getElementById('nav-button').addEventListener('click', () => {
 
-function openSearchForm(){
-    // TODO: ховає меню, показує форму
-}
+    document.getElementById('mobile').className = 'active'
+})
 
+// закриваємо мобільне меню по кліку на кнопку з хрестиком
+document.getElementById('mobile-button').addEventListener('click', () => {
+
+    document.getElementById('mobile').className = ''
+})
+
+// закриваємо мобільне меню, натиснувши ескейп
+document.addEventListener('keydown', event =>{
+
+    if(event.code === 'Escape' || event.key === 'Escape'){
+
+        document.getElementById('mobile').className = ''
+    }
+})
+
+//11-11-21
